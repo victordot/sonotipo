6987fcddc160d88994c5db04fcf7d3aad3143f1a
Lucas Bustamante <125931473+Lucasbte@users.noreply.github.com>
2024-11-09 09:14:22 -0500

    Update page.tsx
    Added Footer

--- app/especies/page.tsx
+++ app/especies/page.tsx
@@ -1,6 +1,7 @@
 // pages/index.tsx o el archivo principal de la página
 import SpeciesTable from "../components/SpeciesTable";
 import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 import Guacamaya from "../../public/assets/Guacamaya.webp"
 
 export default function Home() {
@@ -20,27 +21,28 @@ export default function Home() {
   return (
     <div className="p-4">
       <Navbar />
-    <div className="px-6 py-4">
-      <h2 className="text-2xl font-semibold text-gray-800 dark:text-white mb-6">Lista Especies</h2>
-      <div className="overflow-x-auto bg-white dark:bg-gray-800 rounded-lg shadow-md">
-        <table className="min-w-full table-auto">
-          <thead className="bg-gray-50 dark:bg-gray-700">
-            <tr>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Especie</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Nombre cientifico</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Habitat</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Tipo</th>
-              <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Detalles</th>
-            </tr>
-          </thead>
-          <tbody>
-            {species.map((speciesItem, index) => (
-              <SpeciesTable link={""} key={index} {...speciesItem} />
-            ))}
-          </tbody>
-        </table>
+      <div className="px-6 py-4">
+        <h2 className="text-2xl font-semibold text-gray-800 dark:text-white mb-6">Lista Especies</h2>
+        <div className="overflow-x-auto bg-white dark:bg-gray-800 rounded-lg shadow-md">
+          <table className="min-w-full table-auto">
+            <thead className="bg-gray-50 dark:bg-gray-700">
+              <tr>
+                <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Especie</th>
+                <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Nombre cientifico</th>
+                <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Habitat</th>
+                <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Tipo</th>
+                <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 dark:text-gray-300">Detalles</th>
+              </tr>
+            </thead>
+            <tbody>
+              {species.map((speciesItem, index) => (
+                <SpeciesTable link={""} key={index} {...speciesItem} />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+      <Footer />
     </div>
   );
 }
