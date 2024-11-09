af1515483440e0c65928f665f963be3c48e85fed
Lucas Bustamante <125931473+Lucasbte@users.noreply.github.com>
2024-11-09 09:12:51 -0500

    Update page.tsx
    Added Footer

--- app/grabaciones/page.tsx
+++ app/grabaciones/page.tsx
@@ -1,12 +1,11 @@
-
 import { Navbar } from "../components/Navbar";
-
-
+import { Footer } from "../components/Footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
