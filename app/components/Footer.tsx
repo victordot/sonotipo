fbbc382418784c646a782296f6fca1e878cbf3b3
Lucas Bustamante <125931473+Lucasbte@users.noreply.github.com>
2024-11-09 09:08:16 -0500

    Create Footer.tsx
    Pie de páginas

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return(
+        <footer className="py-4 text-center bg-[#FF9913] text-white w-full">
+            2024 Sonotipos Bioacústicos | Todos los derechos reservados | Acerca de | Contactenos
+        </footer>
+    )
+}
