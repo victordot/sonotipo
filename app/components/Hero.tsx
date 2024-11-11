239a3c790ce0bae14d8a0654acd473b456f8e822
victor <victordanilo801@gmail.com>
2024-11-11 18:31:38 -0500

    16

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -38,21 +38,18 @@ export function Hero() {
                 <div className="flex w-full pt-8 gap-4 flex-wrap">
                     <Link href={"/ecosistemas"}>
                         <button className="bg-[#FF9913] w-full py-4 px-8 text-white rounded-[4px] text-[18px]">
-                        Quiero buscar en una zona geográfica
+                            Quiero buscar en una zona geográfica
                         </button>
                     </Link>
                     <Link href={"/especies"}>
                         <button className="w-full text-[#FF9913] text-[18px] flex items-center justify-center gap-x-4">
-                        Busco una especie particular
+                            Busco una especie particular
                         <span>
                             <Image src={Search} alt="Search" width={28} height={28}/>
                         </span>
                         </button>
                     </Link>
                 </div>
-
-
-
 
                 <div className="flex h-full w-full justify-center pt-10">
                     <div className="flex-col items-center">
